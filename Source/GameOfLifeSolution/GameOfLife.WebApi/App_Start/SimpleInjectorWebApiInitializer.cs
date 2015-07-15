2698314c823b572ac7fd62cc7be691d2e6104293
M. Wurdeman <mwurdeman@gmail.com>
2015-07-15 15:01:57 -0400

    Added SimpleInjector to the webapi project

---
+++ Source/GameOfLifeSolution/GameOfLife.WebApi/App_Start/SimpleInjectorWebApiInitializer.cs
@@ -0,0 +1,30 @@
+﻿[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(GameOfLife.WebApi.App_Start.SimpleInjectorWebApiInitializer), "Initialize")]
+
+namespace GameOfLife.WebApi.App_Start
+{
+    using System.Web.Http;
+    using SimpleInjector;
+    using SimpleInjector.Integration.WebApi;
+
+    public class SimpleInjectorWebApiInitializer
+    {
+        public static void Initialize()
+        {
+            var container = new Container();
+
+            InitializeContainer(container);
+
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            //container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+        }
+
+        private static void InitializeContainer(Container container)
+        {
+            // For instance:
+            // container.RegisterWebApiRequest<IUserRepository, SqlUserRepository>();
+        }
+    }
+}
